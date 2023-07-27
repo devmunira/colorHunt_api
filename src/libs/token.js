e21bae29705da053851cd07cc38c685986b4be67
devmunira <fswfofficial@gmail.com>
2023-07-27 16:54:14 +0600

    Fix auth middleware bug

--- src/libs/token.js
+++ src/libs/token.js
@@ -1,27 +1,28 @@
 
 import jwt from "jsonwebtoken";
 import Token from '../models/AuthToken.js';
-import {add} from "date-fns"
+import {add, addMinutes} from "date-fns"
 import ip from "ip"
 
 export const createToken = async (user) => {
-     console.log(user)
    try {
         const access_token = jwt.sign({...user, issuedIp: ip.address()}, process.env.JWT_PASSWORD , {
-        expiresIn : 10 * 60
+        expiresIn : 2 * 60 
        });
        const refresh_token = jwt.sign({...user, issuedIp: ip.address()}, process.env.JWT_REFRESH_PASSWORD , {
-         expiresIn : 3 * 24 * 60 * 60
+         expiresIn : 5 * 60 
        });
 
        const token = new Token({
             token : refresh_token,
             userId : user._id,
             issuedIp: ip.address(),
-            expiredAt : add(new Date() , {days : 3}).toISOString()
-       })
-      await token.save()
+            expiredAt : addMinutes(new Date() , 3).toISOString()
+       });
 
+
+      await token.save()
+       console.log(access_token, refresh_token)
       return {
         access_token,
         refresh_token
--- src/middleware/auth/authenticateMiddleware.js
+++ src/middleware/auth/authenticateMiddleware.js
@@ -8,72 +8,45 @@ export const authenticationMiddleware = async (req, res, next) => {
     const headers = req.headers['authorization'];
     const access_token = headers && headers.split(' ')[1];
   
-    if (!access_token) {
+    if (!headers) {
       return res.status(401).json({ code: 401, message: 'Please Login First' });
     } else {
-
-
-
       jwt.verify(access_token, process.env.JWT_PASSWORD, async (err, payload) => {
         if (err) {
           if (err.name === 'TokenExpiredError') {
-
-
             const refresh_token = req.headers['x-refresh-token'];
-
             try {
               const decodedRefresh = await jwt.verify(refresh_token, process.env.JWT_REFRESH_PASSWORD);
 
               if(decodedRefresh){
                 const dbToken = await AuthToken.findOne({ token: refresh_token });
-
-
                 if (dbToken && dbToken.issuedIp === ip.address() && dbToken.isActive) {
                   const user =  await User.findById(dbToken.userId);
                   
-                  const { refresh_token: newRefreshToken, access_token: newAccessToken } = createToken(user);
+                  const { refresh_token: newRefreshToken, access_token: newAccessToken } = await createToken(user);
+
                   dbToken.isRevoked = true
                   dbToken.revokedIp = ip.address();
                   await dbToken.save();
-                  req.headers['x-refresh-token'] = newRefreshToken;
-                  req.headers['authorization'] = newAccessToken;
+
+                  req.customHeaders = {
+                      'xrefreshtoken' :  newRefreshToken,
+                      'authorization' :  newAccessToken,
+                  }          
                   req.user = user;
                   return next();
                 } else {
-                  return res.status(401).json({ code: 401, message: 'Invalid Refresh Token' });
+                  return res.status(401).json({ code: 401, message: 'Invalid Refresh Token! Login Again' });
                 }
-  
-  
               }
-             
-
             } catch (err) {
-              return res.status(401).json({ code: 401, message : err.message });
+              return res.status(401).json({ code: 401, message : 'Invalid Refresh Token! Login Again!' });
             }
-
-
-
-          }
-          
+          }         
           else {
-            return res.status(401).json({ code: 401, message: err });
+            return res.status(401).json({ code: 401, message: 'Invalid Access Token!' });
           }
-        } 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        }    
         else {
           if (payload.issuedIp !== ip.address()) {
             return res.status(401).json({ code: 401, message: 'Invalid Access Token!' });
